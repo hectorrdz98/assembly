ea9a7e0d7ec7aa2d977778cc772a837dc5dbc223
Hector Rodriguez <=>
2019-04-06 13:26:50 -0600

    Library 3

--- Libreria/Libreria/Practica1.asm
+++ Libreria/Libreria/Practica1.asm
@@ -13,11 +13,15 @@ include C:\Users\1ZW05LA_RS4\Documents\Code\2019\Assembly\Libreria\Libreria\hd.i
 	msg1 db "El factorial de ",0
 	msg2 db " es: ",0
 	num dword 1
+
+	msg3 db "El valor absoluto de ",0
 .code
 
 start:
 	
 	call example1
+	call Crlf
+	call example2
 
 	hd_exitProgram
 	ret
@@ -34,7 +38,7 @@ example1 proc
 		lea edx, msg2
 		call WriteString
 
-		hd_fact num
+		hd_factorial num
 		call WriteDec
 		call Crlf
 
@@ -44,6 +48,37 @@ example1 proc
 	ret
 example1 endp
 
+
+example2 proc
+	pushad
+	; Example 1: Get the factorial of one number
+	mov ebx,4
+	lea edx, msg3
+	call WriteString
+	mov eax, ebx
+	call WriteInt
+	lea edx, msg2
+	call WriteString
+
+	hd_abs ebx
+	call WriteInt
+	call Crlf
+
+	mov ebx,-4
+	lea edx, msg3
+	call WriteString
+	mov eax, ebx
+	call WriteInt
+	lea edx, msg2
+	call WriteString
+
+	hd_abs ebx
+	call WriteInt
+	call Crlf
+	
+	popad
+	ret
+example2 endp
 
 
 
